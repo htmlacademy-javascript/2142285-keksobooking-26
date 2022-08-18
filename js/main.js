86b85b0bbddafd83dcf1b29653f7e5caa307293f
irok1222 <irokcheese@mail.ru>
2022-08-18 14:04:29 +0300

    Модуль

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {getRandomInt,getRandomPositiveFloat,getRandomArrayElement,getRandomArray} from './until.js'
+
 const getLocation = () => ({ 
     lat:getRandomPositiveFloat(35.65000,35.70000,5),
     lng:getRandomPositiveFloat (139.70000,139.80000,5)
